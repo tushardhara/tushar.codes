8e0018b7b10e43a7eeccbed67ab80be349719907
tushardhara <tushardharaster@gmail.com>
2014-09-12 05:03:25 +0530

    final fix

---
+++ index.php
@@ -0,0 +1,119 @@
+
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Tushar Dhara</title>
+        <meta name="description" content="A web developer available for freelance.">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Facebook Open Graph Meta Tags -->
+        <meta property="og:title" content="Tushar Codes" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://tushar.codes/" />
+        <meta property="og:image" content="http://tushar.codes/tushar_codes.jpg" />
+        <meta property="og:description" content="A web developer available for freelance." />
+
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link href='http://fonts.googleapis.com/css?family=Roboto:300italic,900,100italic,400italic,400,300,500italic,700italic,100,700,500,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <header class="navbar">
+            <div class="container">
+                <ul>
+                    <li><a href="https://github.com/tushardhara" target="_blank">
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="26px" height="26px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+<path d="M131.736,379.684c0,13.526,11.447,20.291,34.334,20.291c20.032,0,30.044-7.021,30.044-21.066
+    c0-13.27-10.923-19.898-32.772-19.898C142.269,359.008,131.736,365.898,131.736,379.684z M426.674,0H85.344
+    C38.406,0,0,38.405,0,85.344v341.313C0,473.625,38.406,512,85.344,512h341.33C473.608,512,512,473.625,512,426.656V85.344
+    C512,38.405,473.609,0,426.674,0z M237.864,204.494c-3.642,1.302-8.844,2.729-15.606,4.293c2.08,5.983,3.123,11.576,3.123,16.777
+    c0,16.646-5.009,31.149-15.022,43.506c-10.015,12.354-22.956,19.703-38.823,22.043c-10.402,1.562-15.604,7.156-15.604,16.779
+    c0,3.381,1.689,6.765,5.072,10.145c4.422,4.943,10.924,8.066,19.509,9.367c37.195,5.721,55.793,21.197,55.793,46.427
+    c0,40.321-24.062,60.48-72.181,60.48c-19.771,0-36.028-3.513-48.772-10.534c-16.129-8.845-24.188-22.761-24.188-41.75
+    c0-21.853,12.096-36.809,36.282-44.869v-0.78c-8.842-5.462-13.263-13.785-13.263-24.972c0-14.566,4.162-23.672,12.484-27.311v-0.781
+    c-8.323-2.862-15.737-9.366-22.24-19.513c-7.282-10.926-10.925-22.63-10.925-35.115c0-18.729,6.635-34.335,19.896-46.82
+    c12.748-11.707,27.964-17.558,45.653-17.558c12.747,0,24.579,3.119,35.505,9.361c12.485,0,26.921-3.12,43.313-9.361L237.864,204.494
+    L237.864,204.494z M301.089,339.497h-44.093c0.521-5.201,0.779-14.042,0.779-26.532V191.619c0-12.227-0.262-20.682-0.779-25.359
+    h44.093c-0.521,4.942-0.78,13.135-0.78,24.582v119.782C300.309,323.891,300.568,333.516,301.089,339.497z M298.159,127.241
+    c-5.332,5.722-11.642,8.581-18.923,8.581c-7.543,0-13.979-2.859-19.313-8.581c-5.333-5.726-8.001-12.486-8.001-20.289
+    c0-8.067,2.668-14.956,8.001-20.683c5.333-5.721,11.771-8.584,19.313-8.584c7.281,0,13.592,2.863,18.923,8.584
+    c5.333,5.728,7.999,12.615,7.999,20.683C306.158,114.755,303.492,121.516,298.159,127.241z M420.845,334.424
+    c-9.627,5.205-21.203,7.803-34.729,7.803c-18.985,0-32.124-6.76-39.405-20.287c-5.465-10.145-8.193-26.14-8.193-47.989v-69.842h0.39
+    v-0.781l-5.854-0.391c-3.382,0-7.806,0.391-13.267,1.171V166.26h19.117v-15.216c0-7.285-0.39-13.14-1.17-17.56h45.259
+    c-0.775,4.94-1.168,10.533-1.168,16.776v15.999h33.942v37.846c-1.299,0-3.705-0.129-7.218-0.389
+    c-3.512-0.261-6.828-0.393-9.949-0.393h-16.776v72.572c0,17.433,5.725,26.146,17.167,26.146c8.062,0,15.348-2.209,21.854-6.632
+    V334.424L420.845,334.424z M160.608,200.203c-16.388,0-24.582,9.627-24.582,28.873c0,17.951,8.194,26.924,24.582,26.924
+    c15.868,0,23.801-9.104,23.801-27.313c0-7.545-1.82-14.047-5.461-19.511C174.525,203.195,168.411,200.203,160.608,200.203z"/>
+</svg>
+                    </a></li>
+                    <li><a href="https://www.facebook.com/tushardharaster" target="_blank">
+                        <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="26px" height="26px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+<path id="Facebook" d="M38.078,22.431c0,2.268,0,12.391,0,12.391H29v15.152h9.078V95h18.649V49.975h12.513
+    c0,0,1.172-7.265,1.74-15.209c-1.629,0-14.183,0-14.183,0s0-8.815,0-10.36c0-1.548,2.033-3.631,4.043-3.631c2.006,0,6.239,0,10.16,0
+    c0-2.063,0-9.191,0-15.774c-5.235,0-11.189,0-13.814,0C37.617,5,38.078,20.167,38.078,22.431z"/>
+</svg>
+                    </a></li>
+                    <li><a href="https://www.linkedin.com/in/tushardhara" target="_blank">
+                        <svg version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="26px" height="26px" viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+<path id="LinkedIn" d="M95,59.727V93H75.71V61.955c0-7.799-2.79-13.121-9.773-13.121c-5.33,0-8.502,3.587-9.897,7.056
+    c-0.509,1.241-0.64,2.967-0.64,4.704V93H36.104c0,0,0.26-52.58,0-58.028h19.295v8.225c-0.039,0.062-0.09,0.128-0.127,0.188h0.127
+    v-0.188c2.563-3.948,7.141-9.588,17.388-9.588C85.483,33.609,95,41.903,95,59.727z M15.919,7C9.318,7,5,11.33,5,17.024
+    c0,5.57,4.193,10.031,10.663,10.031h0.129c6.729,0,10.914-4.46,10.914-10.031C26.579,11.33,22.521,7,15.919,7z M6.146,93h19.289
+    V34.972H6.146V93z"/>
+</svg>
+                    </a></li>
+                </ul>
+                <h1><a href="index.php"><img src="images/logo.png" width="50"></a></h1>
+            </div>
+        </header>
+
+        <section class="first-section">
+            <div class="container">
+                <h2>I like making the web a better place <br/>... One website at a time .</h2>
+                <a href="http://issuu.com/tushardhara/docs/portfolio" target="_blank">My Portfolio &rarr;</a>
+            </div>
+        </section>
+        <section class="testimonial">
+            <div class="container">
+                <h2>Testimonial</h2>
+                <hr class="divider blue">
+                <blockquote>
+                    <p>Tushar is a very nice guy and hard worker that has a great skillset. He always finishes jobs on time and understands what the job requires and what we want. He goes the extra mile to make sure that the work is finished with outstanding quality. I would recommend Tushar to everyone as long as he can still work for us.</p>
+                    <cite>&#8213; David Ashman , Founder of <a href="http://galaxmedia.se/" target="_blank">Galax Media</a></cite>
+                </blockquote>
+            </div>
+        </section>
+
+        <section class="hire"> 
+            <div class="container">
+                <h3>If you want to hire me,please <a href="mailto:tushardharaster@gmail.com" target="_blank">get in touch</a>.</h3>
+            </div>
+        </section>
+
+        <footer>
+            <div class="container">
+                <p class="made"><a href="https://www.google.com/maps/dir/Pearl+Block,+Madinaguda,+Chandanagar,+Hyderabad,+Telangana+500050,+India/''/@17.4921978,78.3043071,13z/data=!3m1!4b1!4m13!4m12!1m5!1m1!1s0x3bcb928a8006a427:0xc937089017e5e95e!2m2!1d78.3386399!2d17.4921189!1m5!1m1!1s0x3bcb928a8006a427:0xc937089017e5e95e!2m2!1d78.3386399!2d17.4921189" target="_blank">Where Am I ?</a></p>
+                <p>&copy; 2014 Tushar Dhara.</p>
+            </div>
+        </footer>
+
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-53494648-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+    </body>
+</html>
